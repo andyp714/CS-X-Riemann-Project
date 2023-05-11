7fa15f2653929256038d52cf2b59b5867c726299
andyp714 <andypauley05@gmail.com>
2023-05-11 10:41:51 -0400

    changed graphics of graph

--- polynomialClass.py
+++ polynomialClass.py
@@ -73,30 +73,35 @@ class Polynomial():
         fig, axes = plt.subplots(2,2)
         fig.tight_layout()
         x = np.linspace(endPoint,startPoint, 100)
-        axes[0,0].plot(x, self.findValue(x))
-        axes[0,1].plot(x, self.findValue(x))
-        axes[1,0].plot(x, self.findValue(x))
-        axes[1,1].plot(x, self.findValue(x))
+        axes[0,0].plot(x, self.findValue(x), 'b-')
+        axes[0,1].plot(x, self.findValue(x), 'r-')
+        axes[1,0].plot(x, self.findValue(x), 'g-')
+        axes[1,1].plot(x, self.findValue(x), 'c-')
         sliceValue = (endPoint - startPoint)/sliceAmount
 
         #Left Riemann Sum Graph
         axes[0,0].set_title("Left Riemann Sum = " + str(round(self.leftRiemannSum(startPoint, endPoint, sliceAmount),4)))
         for i in range(sliceAmount):
-            axes[0,0].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * i), fc='none',ec='k'))
+            axes[0,0].plot(startPoint + sliceValue * i, self.findValue(startPoint + sliceValue * i), 'bo')
+            axes[0,0].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * i), fc=(0,0,1,0.2),ec=(0,0,1,0.5)))
 
         #Right Riemann Sum Graph
         axes[0,1].set_title("Right Riemann Sum = " + str(round(self.rightRiemannSum(startPoint, endPoint, sliceAmount),4)))
         for i in range(sliceAmount):
-            axes[0,1].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * (i+1)), fc='none',ec='k'))
+            axes[0,1].plot(startPoint + sliceValue * (i+1), self.findValue(startPoint + sliceValue * (i+1)), 'ro')
+            axes[0,1].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * (i+1)), fc=(1,0,0,0.2),ec=(1,0,0,0.5)))
         
         #Mid Riemann Sum Graph
         axes[1,0].set_title("Midpoint Riemann Sum = " + str(round(self.midRiemannSum(startPoint, endPoint, sliceAmount),4)))
         for i in range(sliceAmount):
-            axes[1,0].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * (i+0.5)), fc='none',ec='k'))
+            axes[1,0].plot(startPoint + sliceValue * (i+0.5), self.findValue(startPoint + sliceValue * (i+0.5)), 'go')
+            axes[1,0].add_patch(Rectangle((startPoint + sliceValue * i,0), sliceValue, self.findValue(startPoint + sliceValue * (i+0.5)), fc=(0,1,0,0.2),ec=(0,1,0,0.6)))
         
         #Trap Riemann Sum Graph
         axes[1,1].set_title("Trapezoid Riemann Sum = " + str(round(self.trapRiemannSum(startPoint, endPoint, sliceAmount),4)))
         for i in range(sliceAmount):
+            axes[1,1].plot(startPoint + sliceValue * (i), self.findValue(startPoint + sliceValue * (i)), 'co')
+            axes[1,1].plot(startPoint + sliceValue * (i+1), self.findValue(startPoint + sliceValue * (i+1)), 'co')
             xCords = [(startPoint + sliceValue * i), (startPoint + sliceValue * (i+1)), (startPoint + sliceValue * (i+1)), (startPoint + sliceValue * i)]
             yCords = [0, 0, self.findValue((startPoint + sliceValue * (i+1))), self.findValue((startPoint + sliceValue * i))]
             axes[1,1].add_patch(Polygon(xy=list(zip(xCords, yCords)), ec='k', fc='none'))
